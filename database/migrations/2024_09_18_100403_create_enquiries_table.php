f427b4bf0fc36ba36dcedaa3f550c02840711a86
akankshaverm <priyaakanksha0507@gmail.com>
2024-09-18 15:40:53 +0530

    table created

--- database/migrations/2024_09_18_100403_create_enquiries_table.php
+++ database/migrations/2024_09_18_100403_create_enquiries_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('enquiries', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('contact');
+            $table->string('email');
+            $table->string('password');
+           
             $table->timestamps();
         });
     }
